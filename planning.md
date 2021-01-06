efae9a4e7e04ee6df07475ccbc2b93ceef31f673
Netta90 <antobrg90@hotmail.it>
2021-01-06 23:37:31 +0100

    initial commit

---
+++ planning.md
@@ -0,0 +1,40 @@
+# Pianificazione Mio blog
+
+
+
+## Sidebar:
+
+Ho bisogno di una sidebar laterale con un pulsante che si apre e si chiude cliccando su di un bottone.
+
+La Sidebar laterale deve essere grigia grande un 25% rispetto alla pagina totale.
+deve contenere diverse sezioni:
+
+- i dati anagrafici Nome e Cognome.
+- foto 
+- Home (articoli del blog)
+- Profilo
+- contatti
+
+### Sidebar versione mobile :
+
+Deve comparire su tutto la lunghezza della pagina, stessi componenti, la barra su versione mobile deve rimanere chiusa fino al click sul bottone.
+
+# Paginazione : 
+
+Backround completamente grigio con una paginazione a tutto schermo, leggermente centrata.
+Bisognerà posizionare un post per riga.
+
+## i Posts : 
+
+La pagina conterrà i posts, ogni post avrà un __titolo__ e un __Contenuto__.
+
+i posts che verranno contenuti nel blog verranno caricati da un file json a parte.
+
+## i Commenti : 
+
+i commenti verranno poi creati con un programma a parte***
+
+
+
+
+ 
---
+++ posts.js
@@ -0,0 +1,41 @@
+var blog = {
+    currentPage: 0,
+    postsContainer: document.getElementById('container-posts'),
+    posts: null,
+    showPosts: function () {
+        /*Per mostrare i posts devo iterare sull'array */
+        this.posts.forEach(post => {
+            this.createPostHTML(post);
+
+        });
+    },
+
+    createPostHTML: function (post) {
+        let post_Dom = document.createElement('article');
+        post_Dom.setAttribute('class', 'blog-post');
+        post_Dom.setAttribute('id', post.id);
+
+        let title = document.createElement('h3');
+        title.setAttribute('class', 'title');
+        title.innerHTML = post.title;
+        post_Dom.appendChild(title);
+        
+        let contentBody = document.createElement('div');
+        contentBody.setAttribute('class','body');
+        contentBody.innerHTML = post.content;
+        post_Dom.appendChild(contentBody);        
+
+        this.postsContainer.appendChild(post_Dom);
+    },
+
+}
+
+
+async function initPosts() {
+    let response = await fetch('posts.json');
+    let posts = await response.json();
+    blog.posts = posts;
+    blog.showPosts();
+}
+
+initPosts();
