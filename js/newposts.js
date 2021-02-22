526989ad5acb054580d1d2d5c61c0baa6b81959d
Netta90 <antobrg90@hotmail.it>
2021-02-22 09:01:40 +0100

    Funzionalità per mostrare i post

--- js/newposts.js
+++ js/newposts.js
@@ -4,8 +4,8 @@ var blog = {
 
     createCard: function (post) {
         let cardWrapper = document.createElement("div");
-        cardWrapper.setAttribute("class","col");
-        
+        cardWrapper.setAttribute("class", "col");
+
         let cardDom = document.createElement("div");
         cardDom.setAttribute("class", "card Card-style");
         cardWrapper.appendChild(cardDom);
@@ -14,9 +14,9 @@ var blog = {
 
         let image = document.createElement("img");
         image.setAttribute("class", "card-img-top");
-        image.setAttribute("src",post.image);
+        image.setAttribute("src", post.image);
         cardDom.appendChild(image);
-        
+
 
 
         cardDom.appendChild(this.createCardPosts(post));
@@ -26,65 +26,77 @@ var blog = {
     createCardPosts: function (post) {
 
         let postDom = document.createElement("div");
-        postDom.setAttribute("class","card-body");
+        postDom.setAttribute("class", "card-body");
         postDom.setAttribute('id', post.id);
 
         let title = document.createElement('h5');
         title.setAttribute('class', 'card-title');
         title.innerHTML = post.title;
         postDom.appendChild(title);
-        
-        let paragraphPosts =  document.createElement("p");
+
+        let paragraphPosts = document.createElement("p");
         paragraphPosts.setAttribute('class', 'card-text');
-        paragraphPosts.innerHTML =this.extractPostBodyPreview(post);
+        paragraphPosts.innerHTML = this.extractPostBodyPreview(post);
         postDom.appendChild(paragraphPosts);
 
-        let buttom =  document.createElement("a");
+        let buttom = document.createElement("a");
         buttom.setAttribute('class', 'btn btn-primary');
-        buttom.innerHTML="leggi i più"
+        buttom.innerHTML = "leggi i più"
         postDom.appendChild(buttom);
         return postDom;
     },
 
 
-    prendiPosts: function(posts){
+    prendiPosts: function (posts) {
 
-        posts.forEach(post=>{
+        posts.forEach(post => {
             getPost(post);
-            this.createCard(post);
+
         });
-            
+
     },
 
-    extractPostBodyPreview: function(post){
+    extractPostBodyPreview: function (post) {
         let str = post.content;
-        let newStr = str.substring(0,150);
-        return newStr+ "...";
-        
+        let newStr = str.substring(0, 150);
+        return newStr + "...";
     }
-
-
 }
 
 
 async function getPostsIndex() {
     let response = await fetch('posts/index.json');
     let posts = await response.json();
+
     return posts;
-   
-    
 }
 
 async function getPost(slug) {
     let response = await fetch('posts/' + slug + '/post.json');
     let post = await response.json();
     post.image = "posts/" + slug + "/" + post.image;
-    
+
+    return post;
 }
 
+async function getLatestPosts(slugs) {
+    let promises = [];
 
+    slugs.forEach((slug) => {
+        const promise = getPost(slug);
+        promises.push(promise);
+    });
 
+    return await Promise.all(promises);
+}
 
+async function init() {
+    const postsIndex = await getPostsIndex();
 
+    const selectedSlugs = postsIndex.slice(-3);
+    const posts = await getLatestPosts(selectedSlugs);
 
+    posts.forEach((post) => blog.createCard(post));
+}
 
+init().then();
